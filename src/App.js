47d4e1648e88accea64d573ebe777b71fa7bf1e7
unknown <vagner.cfonseca@gmail.com>
2021-05-10 21:38:30 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
 import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import UserList from './View/UserList'
 import UserForm from './View/UserForm'
-import {Button, Icon} from 'react-native-elements'
+import { Button, Icon } from 'react-native-elements'
+import { UsersProvider } from './context/UsersContext'
 
 
-const Stack = createStackNavigator() 
+const Stack = createStackNavigator()
 
-
-
-export default props =>{
-    return(
-        <NavigationContainer>
-            <Stack.Navigator
-                  initialRouterName="UserList"
-                  screenOptions={screenOptions}
-                  >
-                <Stack.Screen 
-                              name ="UserList"
-                              component= {UserList} 
-                              options={() =>{
-                                  return{
-                                      title: 'Lista de usuários',
-                                      headerRight:() =>(
-                                          <Button
-                                            type="clear"
-                                            icon={<Icon name="add" size={25} color="white"/>}
-                                          
-                                          
-                                          
-                                          />
-                                      )
-                                  }
-                              }}
-                              />
-                <Stack.Screen 
-                              name ="UserForm"
-                              component = {UserForm} 
-                              options={{
-                                  title: 'Formulário de usuários'
-                              }}
-                             />
-            </Stack.Navigator>
-        </NavigationContainer>
-
+export default props => {
+    return (
+        <UsersProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="UserList"
+                    screenOptions={screenOptions}>
+                    <Stack.Screen
+                        name="UserList"
+                        component={UserList}
+                        options={({ navigation }) => {
+                            return {
+                                title: "Lista de Usuários",
+                                headerRight: () => (
+                                    <Button
+                                        onPress={() => navigation.navigate("UserForm")}
+                                        type="clear"
+                                        icon={<Icon name="add" size={25} color="white" />}
+                                    />
+                                )
+                            }
+                        }}
+                    />
+                    <Stack.Screen
+                        name="UserForm"
+                        component={UserForm}
+                        options={{
+                            title: "Formulário de Usuários"
+                        }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </UsersProvider>
     )
 }
 
-
 const screenOptions = {
-    headerStyle:{
+    headerStyle: {
         backgroundColor: '#f4511e'
     },
     headerTintColor: '#fff',
-    headerTitleStyle:{
+    headerTitleStyle: {
         fontWeight: 'bold'
     }
 }
--- src/View/UserList.js
+++ src/View/UserList.js
@@ -1,8 +1,84 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React, { useContext } from 'react'
+import { Text, View, FlatList, Alert } from 'react-native'
+import { Button, Icon, ListItem, Avatar } from 'react-native-elements'
+import UsersContext from './data/context/UsersContext'
 
-export default props =>{
-    return(
-        <Text>UserList</Text>
+export default props => {
+
+    const { state, dispatch } = useContext(UsersContext)
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    dispatch({
+                        type: 'deleteUser',
+                        payload: user,
+                    })
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem2({ item: user }) {
+        return (
+                <ListItem 
+                    key={user.id} 
+                    bottomDivider
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    >
+                  <Avatar source={{uri: user.avatarUrl}} />
+                  <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                  </ListItem.Content>
+                </ListItem>
+              )
+    }
+    /* este não funcionou, então criei aa função acima */
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                leftAvatar={{source: {uri: user.avatarUrl}}}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+                rightElement={getActions(user)}
+                onPress={() => props.navigation.navigate('UserForm', user)}
+            />
+        )
+    }
+
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={state.users}
+                renderItem={getUserItem2}
+            />
+        </View>
     )
 }
+
